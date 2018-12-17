8e53aeb86d6c770c933c6ae421b610112313d34b
Philipp Czora <philipp.czora@cloudogu.com>
2018-12-17 17:38:06 +0100

    Link ui-components on mvn run

--- src/main/js/GlobalMailConfiguration.js
+++ src/main/js/GlobalMailConfiguration.js
@@ -1,6 +1,6 @@
-//@flow
+// @flow
 import React from "react";
-import { Title, GlobalConfiguration } from "@scm-manager/ui-components";
+import { Title, Configuration } from "@scm-manager/ui-components";
 import MailConfigurationForm from "./MailConfigurationForm";
 
 type Props = {
@@ -14,7 +14,7 @@ class GlobalMailConfiguration extends React.Component<Props> {
     return (
       <>
         <Title title="Mail Configuration" />
-        <GlobalConfiguration link={link} render={props => <MailConfigurationForm {...props} />} />
+        <Configuration link={link} render={props => <MailConfigurationForm {...props} />} />
       </>
     );
   }
